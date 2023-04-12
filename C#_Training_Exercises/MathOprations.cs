74366b21961efd7d3116d6eedc3ecc3a90d64eff
Ali Hassan <engineralihassan@gmail.com>
2023-04-11 23:26:13 -0700

    Add project files.

---
+++ C#_Training_Exercises/MathOprations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Training_Exercises
+{
+    public class MathOprations
+    {
+      public  int Add(int a, int b)
+        {
+            return a + b;
+        }
+        // Function for Subtract 2 numbers
+       public  int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        // Function for Multiply 2 numbers
+      public  int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        // Function for Divide 2 numbers
+      public  int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+       public void GetResults()
+        {
+           
+            Console.WriteLine("Please Enter First number ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Second number ");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter operator to perform" +
+                " operation (+,-,*,/) and press Enter");
+            string operationType = Console.ReadLine();
+         
+            switch (operationType)
+            {
+                case "+":
+                    Console.WriteLine("The sum of {0} and {1} is : {2}", number1, number2, this.Add(number1, number2));
+                    break;
+                case "-":
+                    Console.WriteLine("The subtraction of {0} and {1} is : {2}", number1, number2, this.Subtract(number1, number2));
+                    break;
+                case "*":
+                    Console.WriteLine("The multiple  of {0} and {1} is : {2}", number1, number2, this.Multiply(number1, number2));
+                    break;
+                case "/":
+                    Console.WriteLine("The Division  of {0} and {1} is : {2}", number1, number2, this.Divide(number1, number2));
+                    break;
+                default:
+                    Console.WriteLine("Please enter right operator  (+,-,*,/) ");
+                    break;
+
+            }
+
+        }
+
+
+
+    }
+}
---
+++ C#_Training_Exercises/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace C__Training_Exercises
+{
+     class Program
+    {
+      
+       public void CheckEven()
+        {
+            Console.WriteLine("Enter any Number to check The Number is Even or Odd");
+            int inputNumber = int.Parse(Console.ReadLine());
+            string result = inputNumber % 2 == 0 ? "The given number is Even" : "The given number  is Odd";
+            Console.WriteLine(result);
+        }
+        public int GetEvarage()
+        {
+            int marksBook1 = 78, marksBook2 = 67, marksBook3 = 89;
+            int average = (marksBook1 + marksBook2 + marksBook3) / 3;
+            Console.WriteLine("Your average Result is {0} % ", average);
+            return average;
+        }
+
+        public void CheckPrime()
+        {
+
+
+
+            Boolean isPrime = true;
+            int inputValue;
+            do
+            {
+                Console.WriteLine("Please Enter a number to check Prime or Not ");
+                inputValue = int.Parse(Console.ReadLine());
+                for (int i = 2; i <= inputValue - 1; i++)
+                {
+                    if (inputValue % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine(isPrime);
+                if (isPrime)
+                {
+                    Console.Write("Number is prime number");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write("Number is not Prime number");
+                    Console.WriteLine();
+                }
+
+
+
+            } while (isPrime);
+
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# World");
+
+            MathOprations mathOprations = new MathOprations();
+            mathOprations.GetResults();
+
+            Program program = new Program();
+            program.CheckEven();
+            Console.WriteLine("Your average Result is {0} % ", program.GetEvarage());
+            program.CheckPrime();
+        }
+    }
+}
