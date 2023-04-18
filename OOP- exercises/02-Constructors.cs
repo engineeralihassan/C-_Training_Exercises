3e171c8a3f106ef79a7dbf116d87e55bf5b3fa66
Ali Hassan <engineralihassan@gmail.com>
2023-04-18 15:10:47 -0700

    Constructors overloading examples are done

---
+++ OOP- exercises/02-Constructors.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP__exercises
+{
+    class Person
+    {
+        string name;
+        string gender;
+        int age;
+
+      public  Person(string name, int age,string gender)
+        {
+            this.name = name;
+            this.gender = gender;
+                this.age=age;
+
+
+        }
+        public void GetDetails()
+        {
+            Console.WriteLine($" {name} {age}  {gender}");
+        }
+    }
+    class Area
+    {
+        private int lenght;
+        private int width;
+        private int area;
+        public Area(int lenght, int width)
+        {
+            this.lenght = lenght;
+            this.width = width;
+
+        }
+        public void GetArea()
+        {
+            this.area = this.width + this.lenght;
+            Console.WriteLine("The area is : {0}" , this.area);
+        }
+    }
+    class Bank1
+    {
+        private int acountNumber;
+        private int balance;
+       public Bank1()
+        {
+           
+        }
+        public Bank1(int acountNumber1 , int balance)
+        {
+            this.acountNumber = acountNumber1;
+            this.balance = balance;
+        }
+        public void WithDraw( int withdrawal)
+        {
+            this.balance=balance-withdrawal;
+            Console.WriteLine(this.balance);
+        }
+
+    }
+
+    public class Vehicle
+    {
+        public string make;
+        public string model;
+
+        public Vehicle( string make , string model )
+        {
+            this.make = make;
+            this.model = model;
+        }
+
+    }
+    class Car : Vehicle
+    {
+        string brand;
+        public Car(string make , string model,string brand ):base(make , model )
+        {
+            this.brand = brand;
+        }
+        public void GetInformation()
+        {
+            Console.WriteLine($"Model {model} Make: {make} Brand : {brand}");
+        }
+         
+    }
+
+
+    public class _02_Constructors
+    {
+     static void Main()
+        {
+            Console.WriteLine("Wellcome to constructors");
+            Person person = new Person( "Ali Hassan" ,21,"Male");
+            person.GetDetails( );
+            Area area = new Area(12 , 34);
+            area.GetArea( );
+            Bank1 bank = new Bank1( );
+            Bank1 bank1 = new Bank1(12345678,34000 );
+            //bank.WithDraw(23000);
+            bank1.WithDraw(22000);
+            Car car = new Car("By tata farari" , "BRV-3" , "2022");
+            car.GetInformation( );
+
+
+
+
+        }
+
+    }
+}
---
+++ OOP- exercises/02.1-Constructors-overloading.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP__exercises
+{
+    public class ConstOverloading
+    {
+        string name;
+        int age;
+       public ConstOverloading()
+        {
+            
+        }
+        public ConstOverloading(string name , int age )
+        {
+            this.age = age;
+            this.name = name;
+        }
+        public void GetDetails()
+        {
+            Console.WriteLine($"name : {name} age : {age}");
+        }
+    }
+    class BankAlHabib
+    {
+        private long acountnumber ;
+        private int balance;
+        private string acountType;
+      public  BankAlHabib(long acountNumber,int balance , string acounttype)
+        {
+            this.acountnumber = acountNumber;
+            this.balance = balance;
+            this.acountType = acounttype;
+            Console.WriteLine($"Acount number : {this.acountnumber}" +
+                $" Balance : {balance} acount type : {acountType} ");
+
+        }
+
+    }
+
+    class Second
+    {
+        private static void Main()
+        {
+            ConstOverloading constOverloading = new ConstOverloading();
+            ConstOverloading constOverloading1 = new ConstOverloading("Ali Hassa" +
+                "n",21);
+            constOverloading1.GetDetails( );
+
+        } 
+    }
+}
