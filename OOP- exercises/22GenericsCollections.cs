253c06cc4c525e18b0d3d7a035787f60529fe1be
Ali Hassan <engineralihassan@gmail.com>
2023-05-01 23:44:10 +0500

    Dictionary and list are done stack and queue we also done the diffrence is only type

--- OOP- exercises/22GenericsCollections.cs
+++ OOP- exercises/22GenericsCollections.cs
@@ -3,6 +3,7 @@
     public class GenericsCollections
     {
         private List<string> list = new List<string>();
+
         private static void Main()
         {
             Console.WriteLine("Generics type collections in c#");
@@ -52,9 +53,50 @@
             countries1.RemoveAt(1);
             Console.WriteLine($"\nRemoveAll Method Removes: {countries.RemoveAll(x => x.Length < 3)} Element(s)");
             countries.RemoveRange(0, 2);
+            // Create new array with 3 elements.
+            string[] array = new string[] { "INDIA", "USA", "UK" };
+            // Copy the array to a List.
+            List<string> copiedList = new List<string>(array);
+            Console.WriteLine("Copied Elements in List");
+            foreach (var item in copiedList)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Dictionaries
+            //Creating a Dictionary with Key and value both are string type
+            Dictionary<string, string> dictionaryCountries = new Dictionary<string, string>();
+            //Adding Elements to the Dictionary using Add Method of Dictionary class
+            dictionaryCountries.Add("UK", "London, Manchester, Birmingham");
+            dictionaryCountries.Add("USA", "Chicago, New York, Washington");
+            dictionaryCountries.Add("IND", "Mumbai, Delhi, Bhubaneswar");
+            Console.WriteLine("Accessing Dictionary Elements using For Each Loop");
+            foreach (KeyValuePair<string, string> KVP in dictionaryCountries)
+            {
+                Console.WriteLine($"Key:{KVP.Key}, Value: {KVP.Value}");
+            }
+            Console.WriteLine($"Key: USA, Value: {dictionaryCountries["USA"]}");
+            Console.WriteLine($"Key: IND, Value: {dictionaryCountries["IND"]}");
+            Dictionary<string, string> dictionaryCountries1 = new Dictionary<string, string>
+            {
+                { "UK", "London, Manchester, Birmingham" },
+                { "USA", "Chicago, New York, Washington" },
+                { "IND", "Mumbai, Delhi, Bhubaneswar" }
+            };
+            //Accessing Dictionary Elements using For Each Loop
+            Console.WriteLine("Accessing Dictionary Elements using For Each Loop");
+            foreach (KeyValuePair<string, string> KVP in dictionaryCountries1)
+            {
+                Console.WriteLine($"Key:{KVP.Key}, Value: {KVP.Value}");
+            }
+            Console.WriteLine("Is PAK Key Exists : " + dictionaryCountries1.ContainsKey("PAK"));
+            Console.WriteLine("Is Srilanka value Exists : " + dictionaryCountries.ContainsValue("Srilanka"));
+            dictionaryCountries.Remove("PAK");
+
+            Console.WriteLine($"Iterating Dictionary Using AsParallel().ForAll Method");
+            dictionaryCountries.AsParallel()
+            .ForAll(entry => Console.WriteLine(entry.Key + " : " + entry.Value));
         }
-
-
 
     }
 }
