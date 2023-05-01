4b08b038e8bfc8601d29e6ebb981bd96a52d77f7
Ali Hassan <engineralihassan@gmail.com>
2023-05-01 19:41:52 +0500

    ArrayList second example is done

--- OOP- exercises/21-Collections.cs
+++ OOP- exercises/21-Collections.cs
@@ -7,6 +7,7 @@ namespace OOP__exercises
     internal class ArrayList1
     {
         private ArrayList personalarray = new ArrayList();
+        public ArrayList evenNumbersList = new ArrayList();
 
         public void AddItem(dynamic item)
         {
@@ -24,6 +25,23 @@ namespace OOP__exercises
         public void PrintList()
         {
             foreach (dynamic item in personalarray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void GetEvenNumbers(ArrayList list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if ((int)list[i] % 2 == 0)
+                {
+                    evenNumbersList.Add(list[i]);
+                };
+            }
+        }
+        public void PrintList1(dynamic list)
+        {
+            foreach (dynamic item in list)
             {
                 Console.WriteLine(item);
             }
@@ -48,6 +66,23 @@ namespace OOP__exercises
             arrayList1.InsertItem(6, 031333324617);
             arrayList1.RemoveItem("Ali Hassan");
             arrayList1.PrintList();
+            ArrayList list = new ArrayList();
+            list.Add(12);
+            list.Add(13);
+            list.Add(12);
+            list.Add(14);
+            list.Add(15);
+            list.Add(16);
+            list.Add(17);
+            list.Add(18);
+            list.Add(19);
+            list.Add(20);
+            list.Add(21);
+            list.Add(22);
+            arrayList1.GetEvenNumbers(list);
+            arrayList1.PrintList1(arrayList1.evenNumbersList);
+
+
 
 
         }
